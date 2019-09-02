95c1e1f0d5e75d7d9f788730bd522be3d93971aa
Aotian Wu <aotianwu@Aotians-MacBook-Pro.local>
2019-09-01 23:34:48 -0400

    much faster version

--- hw1.exs
+++ hw1.exs
@@ -1,53 +1,94 @@
-defmodule MyModule do    
-    def check(order, num, path) do
-        len = Enum.count(order)
-        if rem(len, 2) == 0 do 
-            {list1, list2} = Enum.split(order, div(len,2))
-            if (Enum.at(list1, -1) != 0 || Enum.at(list2, -1) != 0) do
-                fung1 = List.foldl(list1, 0, fn x, acc -> 10 * acc + x end)
-                fung2 = List.foldl(list2, 0, fn x, acc -> 10 * acc + x end)
-                if fung1 <= fung2 && fung1 * fung2 == num do
-                    IO.puts(Enum.join([fung1, fung2], " "))
-                    File.write(path, Enum.join([num, fung1, fung2], " ") <> "\n", [:append])
-                end
-            end
-        end
-    end
+# generate all the enumerations of the given length
+# defmodule Enumerations do
+#     def of(0) do
+#         [[]]
+#     end
 
-    def _should_swap?(list, start, curr) do
-        curr-1 < 0 or not Enum.at(list, curr) in Enum.slice(list, start..(curr-1))
-    end
+#     def of(len) when len > 0 do
+#         for h <- 0..9, t <- of(len-1), do: [h | t]
+#     end
+# end
 
-    def permutation(list, index, n, num, path) do 
-        if index == n do
-            check(list, num, path)
-        else
-            at_index =  Enum.at(list, index)
-            for i <- index..n-1 do
-                at_i = Enum.at(list, i) 
-                if _should_swap?(list, index, i) do
-                    new_list = List.replace_at(list, i, at_index)
-                    new_list = List.replace_at(new_list, index, at_i)
-                    permutation(new_list, index + 1, n, num, path)
-                end
-            end
-        end
-    end
-
-    def isVampire(num, path) do
-        digits = Integer.digits(num)
-        permutation(digits, 0, length(digits), num, path)
-    end
-
+# measure running time
+defmodule Timer do
+  def measure(function) do
+    function
+    |> :timer.tc
+    |> elem(0)
+    |> Kernel./(1_000_000)
+  end
 end
 
 
-result_path = "./result.txt"
-if File.exists?(result_path), do: File.rm(result_path)
-File.touch(result_path)
-MyModule.isVampire(1000174288, result_path)
-MyModule.isVampire(125460, result_path)
-MyModule.isVampire(1254601234, result_path)
+defmodule VampareNumber do    
+
+    # check whether fung1 * fung2 is in the given range
+    defp is_valid?(fung1, fung2, lower, upper) do 
+        cond do
+            # fung2 < fung1 -> false
+            fung1 * fung2 < lower || fung1 * fung2 > upper -> false
+            rem(fung1, 10) == 0 and rem(fung2, 10) == 0 -> false
+            true -> true
+        end
+    end
+
+    def find_in_range(lower, upper) do
+        min_len = length(Integer.digits(lower))
+        max_len = length(Integer.digits(upper))
+        # find numbers in the range with even length
+        num_lens = Enum.filter(min_len..max_len, fn x -> rem(x, 2) == 0 end)
+
+        for num_len<-num_lens do
+            len = div(num_len, 2)
+            # all_enums = Enumerations.of(len) # get all enumerations
+            # all_nums = Enum.map(all_enums, fn list-> List.foldl(list, 0, fn x, acc -> 10 * acc + x end) end)
+
+            for fung1<- trunc(:math.pow(10, len-1))..trunc(:math.pow(10, len))-1 do # iterate for the fung1
+                # create a BEAM process for each fung1. (about 5 times faster after using it)
+                spawn(fn ->
+                    for fung2<- fung1..trunc(:math.pow(10, len))-1 do     # iterate for the fung2  
+                        if is_valid?(fung1, fung2, lower, upper) do       
+                            if Enum.sort(Integer.digits(fung1*fung2)) == 
+                                Enum.sort(Integer.digits(fung1)++Integer.digits(fung2)) do
+                                IO.puts(Enum.join([fung1*fung2, fung1, fung2], " "))
+                            end             
+                        end
+                    end 
+                end
+                )
+            end
+        end
+    end
+end
+
+time = Timer.measure(fn->
+    VampareNumber.find_in_range(1000000000, 2000000000)
+    # IO.inspect(return_list)
+    # flattened_list = return_list |> List.foldl([], &(&1 ++ &2)) |> List.foldl([], &(&1 ++ &2))
+    # IO.inspect(Enum.filter(flattened_list, fn x -> x != nil end))
+    end
+)
+IO.puts(time)
+
+# all_enums = Enumerations.of(3) # get all enumerations
+# all_nums = Enum.map(all_enums, fn list-> List.foldl(list, 0, fn x, acc -> 10 * acc + x end) end)
+# for num<-all_nums, do: IO.puts(num)
+
+
+# Enumerations.of(6)
+
+
+
+# for num<-100000..200000 do
+#     MyModule.isVampire2(num)
+# end
+# result_path = "./result.txt"
+# if File.exists?(result_path), do: File.rm(result_path)
+# File.touch(result_path)
+# MyModule.isVampire(1000174288, result_path)
+# MyModule.isVampire(125460, result_path)
+# MyModule.isVampire(1254601234, result_path)
+# MyModule.isVampire(1254601234789, result_path)
 
 
 
