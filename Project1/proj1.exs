5cb0ed743b5156d14877ed013872b8a055cad271
advaitambeskar <ambeskaradvait@gmail.com>
2019-09-04 23:22:53 -0400

    Updated Proj1.exs to add create robust functionality

--- Project1/proj1.exs
+++ Project1/proj1.exs
@@ -1,9 +1,11 @@
-:observer.start()
 [s1, s2] = System.argv
 {:ok, pid} = VampireNumberReciever.start_link
 
 [n1, n2] = [String.to_integer(s1), String.to_integer(s2)]
 
+[n1, n2] = [min(n1,n2), max(n1,n2)]
+n1 = max(n1, 100)
+n2 = max(n2, 200)
 
 VampireNumber.find_in_range(n1, n2, pid)
 VampireNumberReciever.write(pid)
